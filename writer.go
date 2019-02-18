f4ac7dbd00e0a553b90149990f41fed471c3b1ba
Aren Azibekyan <asazibek@mts.ru>
2019-02-18 17:21:20 +0300

    go mod init

---
+++ writer.go
@@ -0,0 +1,13 @@
+package logs_writer
+
+import (
+	"errors"
+)
+
+type writer struct {
+}
+
+func NewLogsWriter(logsPath string, dateFmt string) (*writer, error) {
+
+	return nil, errors.New("nil")
+}
